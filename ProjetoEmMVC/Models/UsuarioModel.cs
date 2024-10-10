f2272c9f774dae0283b0031d7325a7d55cc66c4a
Lucio neves <luciocecilia20@gmail.com>
2024-10-09 20:09:13 -0400

    feat: Adiciona os modelo de usuario e response

---
+++ ProjetoEmMVC/Models/UsuarioModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjetoEmMVC.Models
+{
+    public class UsuarioModel
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string Email { get; set; }
+
+        public byte[] SenhaHash { get; set; }
+
+        public byte[] SenhaSalt { get; set; }
+
+    }
+}
