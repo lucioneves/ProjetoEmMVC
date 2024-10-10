8ba60db4f710f57664db149661692f14e31e1ae6
Lucio neves <luciocecilia20@gmail.com>
2024-10-09 20:06:47 -0400

    feat: Adiciona os Dtos de cadastrar usuarios e login ja cadastrados

---
+++ ProjetoEmMVC/Dto/UsuarioRegisterDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoEmMVC.Dto
+{
+    public class UsuarioRegisterDto
+    {
+        [Required(ErrorMessage = "Digite o Nome!")] 
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o Sobrenome!")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Digite o Email!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Digite a senha!")]
+        public string Senha { get; set; }
+
+
+        [Required(ErrorMessage = "Digite a Confirmação de Senha!"),
+         Compare("Senha", ErrorMessage = "As senha não estão iguais")]        
+        public string ConfirmaSenha { get; set; }
+    }
+}
