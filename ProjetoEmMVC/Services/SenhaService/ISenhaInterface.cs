43125389ada163ca64d7e9cb66bfb027048e288d
Lucio neves <luciocecilia20@gmail.com>
2024-10-09 20:10:41 -0400

    feat: Adiciona o serviço de senha

---
+++ ProjetoEmMVC/Services/SenhaService/ISenhaInterface.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoEmMVC.Services.SenhaService
+{
+    public interface ISenhaInterface
+    {
+        void CriarSenhaHash(string senha, out byte[] senhaHash, out byte[] senhaSalt);
+   
+        bool VerificaSenha(string senha, byte[] senhaHash, byte[] senhaSalt);
+    }
+}
---
+++ ProjetoEmMVC/Services/SenhaService/SenhaService.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+namespace ProjetoEmMVC.Services.SenhaService
+{
+    public class SenhaService : ISenhaInterface
+    {
+        public void CriarSenhaHash(string senha, out byte[] senhaHash, out byte[] senhaSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                senhaSalt = hmac.Key;
+                senhaHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(senha));
+
+            }
+        }
+
+        public bool VerificaSenha(string senha, byte[] senhaHash, byte[] senhaSalt)
+        {
+            using (var hmac = new HMACSHA512(senhaSalt)) 
+            {
+                var computerdHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(senha));
+                return computerdHash.SequenceEqual(senhaHash);
+            
+            }
+        }
+    }
+}
